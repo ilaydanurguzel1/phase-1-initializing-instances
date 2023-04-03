465fd5d8c036b357592c3aad1420b4f974ed97b9
Ilayda Nur Guzel <ilaydanurguzel1@gmail.com>
2023-04-03 13:29:06 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code here
+
+//1
+class Breakfast{
+    constructor(food,drink){
+        this.food=food;
+        this.drink=drink;
+    }
+}
+
+
+//2
+class Lunch{
+    constructor(salad,soup,drink){
+        this.salad=salad;
+        this.soup=soup;
+        this.drink=drink;
+    }
+}
+
+
+//3
+class Dinner{
+    #dessert;
+    constructor(salad,soup,entree,dessert){
+        this.salad=salad;
+        this.soup=soup;
+        this.entree=entree;
+        this.#dessert=dessert;
+    }
+}
